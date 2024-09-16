1979fd7ee2bf20e6d3d20a164cce96f46506bf93
1911802 <1911802@TE-GDA-D-D21E15>
2024-09-16 08:47:57 +0200

    Dodaj pliki projektów.

---
+++ OZzadanie/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OZzadanie
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ OZzadanie/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace OZzadanie
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int diceRoll = r.Next(1, 7);
+
+            CounterBtn.Text = "Wylosowany rzut: " + diceRoll.ToString();
+            
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
